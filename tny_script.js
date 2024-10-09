ca05a20bd2211a76b66ed1f5bc82c8a5441b20ea
Navpreet2001 <navpreetkaur.lnu4@rdpolytech.ca>
2024-10-09 11:36:39 -0600

    update

--- tny_script.js
+++ tny_script.js
@@ -1,11 +1,33 @@
+"use strict"
+
+
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Tutorial Case
 
    Countdown Clock
-   Author: 
-   Date:   
+   Author: Navpreet
+   Date:2024-10-08 
 
 */
 
+
+
+/* Display the time left until New Years Eve*/
+
+document.getElementById("days").textcontent = "dd";
+document.getElementById("hrs").textcontent = "hh";
+document.getElementById("mins").textcontent = "mm";
+document.getElementById("secs").textcontent = "ss";
+
+
+/*Store the current date and time */
+var currentDay = new Date("May, 23, 2018 14:35:05");
+var dateStr = currentDay.toLocaleDateString();
+var timeStr = currentDay.toLocaleTimeString();
+
+
+/* Display date & time */
+document.getElementById("dateNow").innerHTML = 
+dateStr + "<br/>" + timeStr;
